cad1e464c5c454d978cf572725a9ee521dd3ecbf
web541 <web541php@gmail.com>
2018-12-29 08:50:55 +1100

    Aircraft Locking Capabilities

--- admin/modules/Fltbook/Fltbook.php
+++ admin/modules/Fltbook/Fltbook.php
@@ -18,6 +18,17 @@ class Fltbook extends CodonModule {
       echo 'Check out the sidebar links for more options.';
       echo '</div>';
     }
+    
+    public function moveAircraft() {
+      switch(DB::escape($this->post->action)) {
+        case 'move_aircraft':
+        $this->move_aircraft_post();
+        break;
+      }
+      $this->set('allaircraft', OperationsData::getAllAircraft());
+      $this->set('allairports', OperationsData::getAllAirports());
+      $this->show('fltbook/move_aircraft');
+    }
 
     public function transfer() {
       switch(DB::escape($this->post->action)) {
@@ -50,6 +61,7 @@ class Fltbook extends CodonModule {
         'disabled_ac_allow_book' 			 => DB::escape($this->post->disabled_ac_allow_book),
         'disabled_ac_sched_show' 			 => DB::escape($this->post->disabled_ac_sched_show),
         'show_ac_if_booked' 		 			 => DB::escape($this->post->show_ac_if_booked),
+        'lock_aircraft_location' 		 	 => DB::escape($this->post->lock_aircraft_location),
         'search_from_current_location' => DB::escape($this->post->search_from_current_location),
         'jumpseat_cost'								 => DB::escape($this->post->jumpseat_cost),
         'pagination_enabled'					 => DB::escape($this->post->pagination_enabled),
@@ -71,12 +83,38 @@ class Fltbook extends CodonModule {
       $this->set('message', 'Pilot Transferred Successfully!');
       $this->show('core_success');
     }
+    
+    protected function move_aircraft_post() {
+      // Get Posted Data
+      $reg = strtoupper(DB::escape($this->post->registration));
+      $loc = strtoupper(DB::escape($this->post->location));
+      // Validate Aircraft
+      $aircraft = OperationsData::getAircraftByReg($reg);
+      if ($aircraft == null) {
+          $this->set('message', 'Invalid Aircraft Registration!');
+          $this->show('core_error');
+          return;
+      }
+      // Validate Airport
+      $airport = OperationsData::getAirportInfo($loc);
+      if ($airport == null) {
+          $this->set('message', 'Invalid Airport ICAO!');
+          $this->show('core_error');
+          return;
+      }
+      // Update Aircraft Location
+      FltbookData::updateAircraftLocation($aircraft->id, $airport->icao);
+      // Return
+      $this->set('message', 'Transferred '.$reg.' to '.$loc.'');
+      $this->show('core_success');
+    }
 
     public function getSettings() {
       return array(
         'disabled_ac_allow_book'       => FltbookData::getSettingByName('disabled_ac_allow_book')->value,
         'disabled_ac_sched_show'       => FltbookData::getSettingByName('disabled_ac_sched_show')->value,
         'show_ac_if_booked' 	       => FltbookData::getSettingByName('show_ac_if_booked')->value,
+        'lock_aircraft_location' 	   => FltbookData::getSettingByName('lock_aircraft_location')->value,
         'search_from_current_location' => FltbookData::getSettingByName('search_from_current_location')->value,
         'jumpseat_cost'		           => FltbookData::getSettingByName('jumpseat_cost')->value,
         'pagination_enabled'	       => FltbookData::getSettingByName('pagination_enabled')->value,
